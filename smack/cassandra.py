62662ce768c39469d90a4100ee8316afc6c77631
Nicolas Simon <simon.nicolas@outlook.be>
2020-01-25 11:24:41 +0000

    Moved scripts to practice repo.

--- smack/cassandra.py
+++
@@ -1,8 +0,0 @@
-#SCYLLA / CASSANDRA
-from cassandra.cluster import Cluster
-#cluster = Cluster(['scylla'])
-cluster = Cluster(['cassandra'])
-session = cluster.connect('')
-rows = session.execute('SELECT * FROM system_schema.keyspaces;')
-for user_row in rows:
-    print(user_row[0])
